858f90e16f5d681b711fc1e2a124691f99dfa441
Faiza Abdul-Wahab <faizaabdulwahab@ymail.com>
2024-09-30 16:36:42 +0000

    first-project

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+       
+      <section>
+         <nav className='navitems'>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Certificate</a>
+          <a href="#">Projects</a>
+        </nav>
+      <div className='hero'>
+      
+      <h1>Hello React and frontend class!</h1>
+  <p>This is a description</p>
+        </div> 
+      </section>
+      
+
+        </div>
+      
+ 
+  
+ 
+  )
+}
+
+export default App
